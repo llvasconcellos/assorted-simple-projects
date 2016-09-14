b1da6ab04e3cc9417760b0bcb0f9b6103014cee9
Leonardo Lima de Vasconcellos <leonardo@devhouse.com.br>
2016-09-14 00:06:57 -0300

    clave de sol unfinished

---
+++ clave-de-sol/www/index.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+<title>.:: Clave De Sol.com.br ::. Instrumentos Usados</title>
+<link type="text/css" href="includes/estilo.css" rel="stylesheet">
+</head>
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center" valign="top"> 
+      <table width="775" border="1" cellpadding="0" cellspacing="0" bgcolor="#FCECDC">
+        <tr> 
+          <td colspan="3" valign="top">
+			<table width="100%" border="1" cellspacing="0" cellpadding="0">
+              <tr> 
+                <td rowspan="2"><img src="img/logo.gif"></td>
+                <td colspan="2" valign="top"><img src="img/cabecalho.jpg"></td>
+              </tr>
+              <tr> 
+                <td>BANNER</td>
+                <td>&nbsp;</td>
+              </tr>
+            </table> </td>
+        </tr>
+        <tr> 
+          <td class="menuesquerdo"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <td class="opcaomenuinstrumentos">Categorias</td>
+                    </tr>
+                    <tr>
+                      <td class="opcaomenuinstrumentos">Cordas</td>
+                    </tr>
+                    <tr>
+                      <td class="opcaomenuinstrumentos">Sopro</td>
+                    </tr>
+                    <tr>
+                      <td class="opcaomenuinstrumentos">Percursão</td>
+                    </tr>
+                    <tr>
+                      <td class="opcaomenuinstrumentos">Eletrônicos</td>
+                    </tr>
+                    <tr>
+                      <td class="opcaomenuinstrumentos">Outros</td>
+                    </tr>
+                  </table></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+            </table></td>
+          <td class="conteudo">&nbsp;fdsafdsa fdsa fdsa fdsa fdsafdsa fdsa fds<br>
+            fdsa fdsa fdsa fdsa fdsa fdsaf dsafdsa fds af dsaf dsa fdsa fdsa f 
+            fdsaf dsa fdsa fdsa fdsa fdsa fdsa fds</td>
+          <td class="menudireito"> <table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr> 
+                <td>&nbsp;</td>
+              </tr>
+              <tr> 
+                <td>&nbsp;</td>
+              </tr>
+              <tr> 
+                <td>&nbsp;</td>
+              </tr>
+            </table></td>
+        </tr>
+        <tr> 
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+</html>
